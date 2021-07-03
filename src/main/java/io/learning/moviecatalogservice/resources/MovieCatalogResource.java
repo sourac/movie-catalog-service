b1b5af397dd2850478ebee5de8ef87f7bcd50672
Atul Sharma <atulsharma21772@gmail.com>
2021-07-04 00:20:19 +0530

    starting with the resources. now with the model and the mapping of the endpoint.

--- src/main/java/io/learning/moviecatalogservice/resources/MovieCatalogResource.java
+++ src/main/java/io/learning/moviecatalogservice/resources/MovieCatalogResource.java
@@ -1,7 +1,23 @@
 package io.learning.moviecatalogservice.resources;
 
+import io.learning.moviecatalogservice.model.CatalogItem;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+import java.util.List;
+
 @RestController
+@RequestMapping("/catalog")
 public class MovieCatalogResource {
+
+    @RequestMapping("/{userId}")
+public List<CatalogItem> getCatalogItem(@PathVariable("userId") String userId){
+
+    return Collections.singletonList(
+      new CatalogItem("Transformers","Test",4)
+    );
+  }
+
 }
